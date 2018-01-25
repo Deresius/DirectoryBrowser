4d1ccef2565f9ecab56166a4bf569e8a32a3ab2d
David Spurlock <DavidMichaelSpurlock@live.com>
2018-01-24 20:25:40 -0500

    Added Directory ListView and changed constraint to LinearLayout

--- app/src/main/java/com/example/david/directorybrowser/MainActivity.java
+++ app/src/main/java/com/example/david/directorybrowser/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     private void TestUpload() {
