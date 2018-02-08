bba434e954882eafbeb426b03a0d957f9bb7ae44
Jacob Slattery <timberwolf@bellsouth.net>
2018-02-08 18:15:52 -0500

    Fixed code problems

--- app/src/main/java/com/example/david/directorybrowser/MainActivity.java
+++ app/src/main/java/com/example/david/directorybrowser/MainActivity.java
@@ -86,11 +86,7 @@ public class MainActivity extends AppCompatActivity {
 
                 requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
                         1);
-            } else {
-
             }
-
-
         } else {
             setupPage();
         }
@@ -104,10 +100,7 @@ public class MainActivity extends AppCompatActivity {
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     setupPage();
-                } else {
-
                 }
-                return;
             }
 
         }
